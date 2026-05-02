42d5249d33b3c3886108f2676faae5ed54fe7568
wizeoQ <quartetqoc@gmail.com>
2026-05-02 22:50:29 +0300

    Создать класс Vizualizer

--- radio.py
+++ radio.py
@@ -1,27 +1,11 @@
-import numpy as np
+import matplotlib.pyplot as plt
 
-
-# 5.4 - разработать проект симулятор радиоканала
-# 5.1 - создать классы для генерации сигналов и их модуляци
-# 5.4.1.1 Класс SignalGenerator
-class SignalGenerator:
-    def __init__(self, frequancy, amplitude = 1.0):
-        self.frequancy = frequancy
-        self.amplitude = amplitude
-        
-    def genetate(self, time):
-        return self.amplitude * np.sin(2 * np.pi * self.frequancy * time)
-
-# 5.4.1.2 Класс Modulator
-class Modulator:
-    def __init__(self, carrier_frequancy):
-        self.carrier_frequancy = carrier_frequancy
-    
-    def am_modulate(self, signal, time):
-        carrier = np.sin(2 * np.pi * self.carrier_frequancy * time)
-
-# 5.4.2. Расширение функционала классов:
-# 5.4.2.1. Добавление других видов модуляции
-    def fm_modulate(self, signal, time, modulation_index=1.0):
-        phase = 2 * np.pi * self.carrier_frequancy * time + modulation_index * signal
-        return np.sin(phase)
+class Visualizer:
+    def plot_signal(self, time, signal, title="Signal"):
+        plt.figure()
+        plt.plot(time, signal)
+        plt.title(title)
+        plt.xlabel("Time")
+        plt.ylabel("Amplitude")
+        plt.grid()
+        plt.show()
