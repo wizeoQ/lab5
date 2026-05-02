7d49f441ef6fd64a1413ddaaf44e5c88cb7f16e0
wizeoQ <quartetqoc@gmail.com>
2026-05-02 22:45:16 +0300

    Добавить частотную модуляцию fm_modulate

--- radio.py
+++ radio.py
@@ -19,3 +19,9 @@ class Modulator:
     
     def am_modulate(self, signal, time):
         carrier = np.sin(2 * np.pi * self.carrier_frequancy * time)
+
+# 5.4.2. Расширение функционала классов:
+# 5.4.2.1. Добавление других видов модуляции
+    def fm_modulate(self, signal, time, modulation_index=1.0):
+        phase = 2 * np.pi * self.carrier_frequancy * time + modulation_index * signal
+        return np.sin(phase)
